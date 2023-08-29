603b8c58f08f86442d5a93b7cecbbae2c292d5a6
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-08-28 23:15:55 -0300

    fix: url again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon2.ico",
 
   // Set the production url of your site here
-  url: "https://rinyaresu.github.io/ruby4noobs/",
+  url: "https://rinyaresu.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/ruby4noobs",
